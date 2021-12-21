f68a90e23e1ab58b9403518cf1aa72316f4291cb
MarkL <3318567+MarkLWasTaken@users.noreply.github.com>
2021-12-22 01:51:23 +0800

    New save instance. (MarkL_Build_41_61_MP_CEO_4)

---
+++ project-zomboid-build-41-60/Server02-CEO-Edition-4/AdditionalFiles/README.md
@@ -0,0 +1,4 @@
+# Additional Files
+All the additional files as requested and required by the game server.<br>
+<br>
+Nothing here for the time being.
---
+++ project-zomboid-build-41-60/Server02-CEO-Edition-4/Server/MarkL_Build_41_61_MP_CEO_3_spawnregions.lua
@@ -0,0 +1,14 @@
+function SpawnRegions()
+	return {
+		{ name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
+		{ name = "Riverside, KY", file = "media/maps/Riverside, KY/spawnpoints.lua" },
+		{ name = "Rosewood, KY", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
+		{ name = "West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
+		{ name = "RavenCreek, KY", file = "media/maps/RavenCreek/spawnpoints.lua" },
+		{ name = "Save Our Station!, KY", file = "media/maps/SaveOurStation_KnoxCountry_World/spawnpoints.lua" },
+		{ name = "Grapeseed,KY", file = "media/maps/Grapeseed/spawnpoints.lua" },
+		{ name = "Lake Ivy Township, KY", file = "media/maps/li_township/spawnpoints.lua" },
+		{ name = "Gas Station, KY", file = "media/maps/Gas Station/spawnpoints.lua" },
+		{ name = "Park Rangers HQ, KY", file = "media/maps/hq/spawnpoints.lua" },
+	}
+end
